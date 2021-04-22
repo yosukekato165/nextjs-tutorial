e8461dd188b12ffb2fd1cccdea63a07bd1ad9760
yosukekato165 <padyk165@gmail.com>
2021-04-22 22:55:55 +0900

    next.js tutorial

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,11 @@
-import Layout from '../../components/layout'
+import Head from 'next/head'
+
 import { getAllPostIds, getPostData } from '../../lib/posts'
+
+import Layout from '../../components/layout'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+
 
 
 export async function getStaticPaths() {
@@ -19,16 +25,20 @@ export async function getStaticProps({ params }) {
   }
 }
 
+
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
